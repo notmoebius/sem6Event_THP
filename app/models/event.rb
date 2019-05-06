2ff5038195c6a79e20c9fbac0d29e25e51fdd1e7
notmoebius <notmoebius@gmail.com>
2019-05-06 16:23:37 +0200

    update with mailer

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,18 +1,32 @@
 class Event < ApplicationRecord
   has_many :attendances
   has_many :users, through: :attendances
+  belongs_to :administrator, class_name: 'User'
+  #validate :duration_modulo_5
+  #validate :start_date_cannot_be_in_the_past
 
-  validates :start_date, presence: true
-  validates :duration, presence: true, numericality: true
+  validates :start_date, presence: true 
+   
+  validates :duration, presence: true
+  
   validates :description, presence: true, length: { in: 20..1000, message: "description need 20 car. min and 1000 car. max"} 
+  
   validates :title, presence: true, length: { in: 5..140, message: "title need 5 car. min and 140 car. max"} 
+  
   validates :price, presence: true, numericality: { in: 1..1000, }
+  
   validates :location, presence: true
+ 
+  # private
+  # def start_date_cannot_be_in_the_past
+  #   if start_date < Time.now
+  #     errors.add("can't be in the past")
+  #   end
+  # end
 
-  private
-  def start_date_cannot_be_in_the_past
-    if start_date.present? && start_date < Date.today
-      errors.add(:start_date, "can't be in the past")
-    end
-  end  
+  # def duration_modulo_5
+  #   if !(duration % 5 == 0 && duration >= 5)
+  #     errors.add("must be multiple of 5")
+  #   end
+  # end
 end
