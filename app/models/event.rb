8e764c68e7a02495162191782f96f23258ff9c90
notmoebius <notmoebius@gmail.com>
2019-05-06 12:48:38 +0200

    fix validates

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
     validates :start_date, presence: true
-    validates :duration, presence: true, numericality: { :greater_than 0, message: "More than 0" }
+    validates :duration, presence: true, numericality: true
     validates :description, presence: true, length: { in: 20..1000, message: "description need 20 car. min and 1000 car. max"} 
     validates :title, presence: true, length: { in: 5..140, message: "title need 5 car. min and 140 car. max"} 
     validates :price, presence: true, numericality: { in: 1..1000, }
