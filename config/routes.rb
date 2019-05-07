d4e7bba426ead8d4f08b79e666373583004f1f46
notmoebius <notmoebius@gmail.com>
2019-05-07 14:36:48 +0200

    add routes events and user#show, controller events and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'events#index'
   devise_for :users
+  resources :events, only: [:show, :new, :index]
+  resources :users, only: [:show]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
